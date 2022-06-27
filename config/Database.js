77e4450e576a8db2b87479d494b4fe904c4d422e
Triyono <yono.tri@gmail.com>
2022-06-27 09:23:26 +0700

    change db mysql heroku

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,10 @@
 import {Sequelize} from "sequelize";
  
-const db = new Sequelize('crud_db','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
+// const db = new Sequelize('crud_db','root','',{
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+const db = new Sequelize('mysql://b91f2417e93c34:f90f1860@us-cdbr-east-05.cleardb.net/heroku_fbfe348d564b3ac?reconnect=true');
  
 export default db;
  
