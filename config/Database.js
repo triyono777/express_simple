9b54974eae018669b1b0b5266fae292195e8b053
Triyono <yono.tri@gmail.com>
2022-06-27 09:33:11 +0700

    db heroku mysql

--- config/Database.js
+++ config/Database.js
@@ -4,6 +4,7 @@ import {Sequelize} from "sequelize";
 //     host: 'localhost',
 //     dialect: 'mysql'
 // });
+
 const db = new Sequelize('mysql://b91f2417e93c34:f90f1860@us-cdbr-east-05.cleardb.net/heroku_fbfe348d564b3ac?reconnect=true');
  
 export default db;
